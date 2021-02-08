267b44844d0bcba3078708062e9915ecb24e6935
Cleiton366 <cleiton0050cf@gmail.com>
2021-02-08 19:25:46 -0300

    my-account page

---
+++ js/effects/openImgUser.js
@@ -0,0 +1,8 @@
+function openImgUser() {
+    document.getElementById("send-user-img").style.marginLeft = "500px";
+    document.getElementById("user-info").style.opacity = "0";
+}
+function closeImgUser() {
+    document.getElementById("send-user-img").style.marginLeft = "-500px";
+    document.getElementById("user-info").style.opacity = "1.0";
+}
